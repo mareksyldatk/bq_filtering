Fixed some numerical problems in Quadrature Integration

BQF_Scenario_3.py
@@ -25,20 +25,20 @@ import GPy
 
 
 # Set random SEED:    
-RAND_SEED = 8
+RAND_SEED = 997
 random.seed(RAND_SEED)
 np.random.seed(RAND_SEED)
 
 # Parameters:
 N_TIME_STEPS = 50
 KAPPA = 2.0
-N_PARTICLES = 2500
+N_PARTICLES = 1000
     
 # Define model:
 model = bqf.SystemModel(f_type='nonlinear', h_type='nonlinear')
 
 def function_f(x_k, k):
-    x_ = 0.5*x_k + 25*(x_k/(1+(x_k**2))) + 8*np.cos(1.2*k)
+    x_ = 0.5*x_k + 25.0*(x_k/(1+(x_k**2))) + 8.0*np.cos(1.2*k)
     x_ = np.array([1.0]) * x_
     return x_
     
@@ -48,7 +48,7 @@ def function_h(x_k, k):
     return y_
 
 def derivative_f(x_k, k):
-    dfdx = 0.5 + 25 * ( ( 1-x_k**2 )/( (1+x_k**2)**2 ) )
+    dfdx = 0.5 + 25.0 * ( ( 1-x_k**2 )/( (1+x_k**2)**2 ) )
     dfdx = np.array([1.0]) * dfdx
     return dfdx
 
@@ -57,23 +57,23 @@ def derivative_h(x_k, k):
     dhdx = np.array([1.0]) * dhdx
     return dhdx    
 
+# Seat measurement functions and their derivatives:
 model.fun_f = function_f
 model.fun_h = function_h 
-
 model.der_f = derivative_f
 model.der_h = derivative_h
-
+# Determine B and G matrices:
 model.B = np.array([[1.0]])
 model.G = np.array([[1.0]])
-
+# Set noise covariances:
 model.Q = np.diag([1.0])
 model.R = np.diag([10.0])
 
 # Initial conditions:
-x0 = np.array([[0]])
-p0 = np.diag([5])
+x0 = np.array([[0.0]])
+p0 = np.diag([5.0])
 
-# Define states and observationsA
+# Define states and observations:
 X_noise = np.random.multivariate_normal(np.array([0]), model.Q, N_TIME_STEPS)
 Y_noise = np.random.multivariate_normal(np.array([0]), model.R, N_TIME_STEPS)
 X_true  = x0
@@ -98,26 +98,32 @@ pf = bqf.ParticleFilter(model, n_particles=N_PARTICLES)
 pf_X_, X__, P_, P__ = pf.filtering(x0, p0, Y)
     
 ''' ----- QF ----- '''
-N_SAMPLES = s
+N_SAMPLES = 2
 KERNEL    = GPy.kern.RBF(input_dim = 1, ARD=True)  
-OPT_PAR = {"MAX_T": 100, "MAX_F": 100}
+OPT_PAR = {"MAX_T": 100, "MAX_F": 333}
 
 def K_CONST(gp):
     NUM_RESTARTS = 16
     
     gp.rbf.variance.constrain_fixed(1.0, warning=False)        
-    gp.rbf.lengthscale.constrain_fixed(2.0, warning=False)
+    gp.rbf.lengthscale.constrain_fixed(3.0, warning=False)
     gp.Gaussian_noise.variance.constrain_fixed(0.0001**2, warning=False) 
     
     return(gp, NUM_RESTARTS)
     
-#qf = bqf.QuadratureFilter(model, KERNEL, N_SAMPLES, K_CONST,  OPT_PAR)
-#qf_X_, qf_X__, qf_P_, qf_P__ = qf.filtering(x0, p0, Y)    
+qf = bqf.QuadratureFilter(model, KERNEL, N_SAMPLES, K_CONST,  OPT_PAR)
+qf_X_, qf_X__, qf_P_, qf_P__ = qf.filtering(x0, p0, Y)    
 
 # Plot filtering results    
 pp.figure()
-pp.plot(X_true, '-k')
+pp.plot(X_true,'-k')
 pp.plot(kf_X_, '-r')
-pp.plot(ukf_X_, '--g')
+pp.plot(ukf_X_,'--g')
 pp.plot(pf_X_, '.-b')
-#pp.plot(qf_X_, '.-m')
+pp.plot(qf_X_, '.-m')
+
+# Print RMSE:
+print ("RMSE for EKF: " + str( np.sqrt(np.sum(np.power((X_true- kf_X_),2))/N_TIME_STEPS) ) )
+print ("RMSE for UKF: " + str( np.sqrt(np.sum(np.power((X_true-ukf_X_),2))/N_TIME_STEPS) ) )
+print ("RMSE for PF:  " + str( np.sqrt(np.sum(np.power((X_true- pf_X_),2))/N_TIME_STEPS) ) )
+print ("RMSE for QF:  " + str( np.sqrt(np.sum(np.power((X_true- qf_X_),2))/N_TIME_STEPS) ) )

BayesianQuadratureFiltering.py
@@ -328,7 +328,7 @@ class ParticleFilter(object):
             p__ = symetrize_cov(p__)
             #
             # 2. Update
-            G = self.model.getG() #TODO
+            G = self.model.getG()
             R = self.model.R
             yi = np.apply_along_axis(self.model.h, 1, xi, k=k)
             wi = np.apply_along_axis(scipy.stats.multivariate_normal(Y[k], G.dot(R).dot(G.T)).pdf, 1, yi)
@@ -428,15 +428,20 @@ class QuadratureFilter(object):
             
             #
             # 3. Get estimates
-            K = mo.mrdivide(C, S)
-            #TODO: K = C.dot(np.linalg.inv(S))
             eps = Y[k] - to_row(mu)
             eps = to_column(eps)
+            
+            # TODO: Version with K computed
+            # K = mo.mrdivide(C, S)
+            # K = C.dot(np.linalg.inv(S))
+            # x_ = x__ + K.dot(eps)
+            # p_ = p__ - K.dot(S).dot(K.T)
+            # p_ = symetrize_cov(p_)
 
-            #TODO: x_ = x__ + K.dot(eps)
+            # TODO: Version without direct computation of K
             x_ = x__ + C.dot(mo.mldivide(S, eps))
-            p_ = p__ - K.dot(S).dot(K.T)
-            p_ = symetrize_cov(p_)
+            p_ = p__ - C.dot(mo.mrdivide(C,S).T)
+            p_ = symetrize_cov(p_)       
             
             #
             # 4. Save results
@@ -555,11 +560,16 @@ class QuadratureFilter(object):
         
         # Initiale Sigma and CC
         Sigma_ = CC_ = None     
-
-        # Compute mean, cov matrix and cross cov matrix
+        
+        # TODO: Sigma computation as closed form
+        Sigma_ = w_0/np.sqrt(np.linalg.det( 2*mo.mldivide(A,P) + I) ) - (z.T).dot(mo.mldivide(K,z))
+        
+        # Compute cov matrix and cross cov matrix
         for i in range(0,len(W)):
-            YY_i   = ( to_column(Y[i]-mu_) ).dot( to_row(Y[i]-mu_) )
-            Sigma_ = W[i] * YY_i if i == 0 else Sigma_ + W[i] * YY_i
+        
+            # TODO: Sigma computation as sumation
+            # YY_i   = ( to_column(Y[i]-mu_) ).dot( to_row(Y[i]-mu_) )
+            # Sigma_ = W[i] * YY_i if i == 0 else Sigma_ + W[i] * YY_i
             
             XY_i = ( to_column(X[i]-m) ).dot( to_row(Y[i]-mu_) )
             CC_  = W[i] * XY_i if i == 0 else CC_ + W[i] * XY_i